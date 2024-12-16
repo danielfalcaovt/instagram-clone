5fa92db819009a6d4fd25e2dd0ebd2f0c9ac3286
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-16 17:33:46 -0300

    feat: ensure HomePage render FeedContainer with correct styles

--- src/view/pages/home-page.tsx
+++ src/view/pages/home-page.tsx
@@ -1,8 +1,13 @@
-import React from "react"
+import React, { JSX } from "react"
 import Aside from "../components/aside/aside"
+import { FeedContainer } from "../components/activity-feed/feed-container"
+import * as styles from './home-page-styles.scss'
 
-export default function HomePage() {
+export default function HomePage(): JSX.Element {
   return (
-    <Aside/>
+    <div className={styles.homeContainer}>
+      <Aside/>
+      <FeedContainer/>
+    </div>
   )
 }
