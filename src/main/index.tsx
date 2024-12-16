38b7299ca43c575a35c94fc7e7b39a37c2d1e7e0
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-16 13:32:18 -0300

    feat: ensure HomePage render Aside

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './config/app'
+import '@/view/styles/global-styles.scss'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
--- src/view/pages/home-page.tsx
+++ src/view/pages/home-page.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import Aside from "../components/aside/aside"
 
 export default function HomePage() {
   return (
-    <h1>Home Page</h1>
+    <Aside/>
   )
 }
