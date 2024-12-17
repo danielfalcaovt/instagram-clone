b3822858d8a4c08c9da53559a9fc29480bbf9474
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-17 15:51:42 -0300

    fix: days passed since post creation calculation

--- src/application/usecases/user-post/get-many-user-post-by-user.ts
+++ src/application/usecases/user-post/get-many-user-post-by-user.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { IRemoteGetManyUserPostByUser } from "@/application/protocols/remote-get-many-user-post-by-user";
 import { UserPost } from "@/domain/protocols/user/post";
 import { GetManyPostsByUser, GetUserPostModel } from "@/domain/usecases/posts/get-many-posts-by-user";
@@ -8,28 +9,31 @@ export class GetManyUserPostByUser implements GetManyPostsByUser {
     const result: GetUserPostModel[] = []
     const posts = await this.remoteGetManyUserPostByUser.getManyBy(userId)
     posts.map((post: UserPost) => {
+      console.log(post.created_at)
       result.push({
         ...post,
         time_moved: this.howMuchTimeMoved(post.created_at)
       })
     })
+    console.log(result[0].time_moved)
     return result
   }
 
   howMuchTimeMoved(sinceDate: Date): string {
     const now = new Date()
-    const howMuchTimeMovedInMs = now.getTime() - sinceDate.getTime()
-    const howMuchTimeInDays = howMuchTimeMovedInMs / (1000 * 60 * 60 * 12)
-    const timeMovedInMS = new Date(howMuchTimeMovedInMs)
+    const howMuchTimeMovedInMs = (now as any) - (sinceDate as any)
+    const howMuchTimeInDays = Math.floor(howMuchTimeMovedInMs / (1000 * 60 * 60 * 24))
+    console.log(howMuchTimeInDays)
     if (howMuchTimeInDays >= 365) {
-      const yearsMoved = timeMovedInMS.getFullYear() - now.getFullYear()
+      const yearsMoved = now.getFullYear() - sinceDate.getFullYear()
       return `${yearsMoved}y`
     } else if (howMuchTimeInDays >= 31) {
-      const monthsMoved = timeMovedInMS.getMonth() - now.getMonth()
+      const monthsMoved = now.getMonth() - sinceDate.getMonth()
       return `${monthsMoved}m`
     } else if (howMuchTimeInDays >= 7) {
-      const weeksMoved = timeMovedInMS.getDate()
-      return `${weeksMoved > 28 ? 4 : weeksMoved > 21 ? 3 : weeksMoved > 14 ? 2 : 1}w`
+      return `${howMuchTimeInDays > 28 ? 4 : howMuchTimeInDays > 21 ? 3 : howMuchTimeInDays > 14 ? 2 : 1}w`
+    } else {
+      return howMuchTimeInDays > 0 ? `${howMuchTimeInDays}d` : 'now'
     }
   }
 }
