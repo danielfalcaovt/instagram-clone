da12f3ddf161d2b9d798a7a3c5e88eb8ecc1477f
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-17 14:24:34 -0300

    feat: ensure makeHomePage factory creates correctly home page

--- src/main/factory/home-page/home-page.factory.tsx
+++ src/main/factory/home-page/home-page.factory.tsx
@@ -1,6 +1,9 @@
 import React, { JSX } from "react";
 import HomePage from "@/view/pages/home-page";
+import { GetManyUserPostByUser } from "@/application/usecases/user-post/get-many-user-post-by-user";
+import { HttpClientMock } from "@/infra/http-client/http-client-mock";
 
 export default function makeHomePage(): JSX.Element {
-  return <HomePage/>
+  const dbGetManyPostsByUser = new GetManyUserPostByUser(new HttpClientMock())
+  return <HomePage DbGetManyPostsByUser={dbGetManyPostsByUser}/>
 }
